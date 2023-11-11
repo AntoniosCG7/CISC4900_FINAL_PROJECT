7149324eb0e7d488a90ceac04e1254717f12aa84
Antonios Chatzinikolas <antonis_132000@hotmail.com>
2023-11-11 04:51:19 -0500

    Refactor Google Maps integration with new library
    - Migrate to @react-google-maps/api for Google Maps API integration
    - Update map component to align with new library usage

---
+++ LinguaConnect/client/src/components/UIElements/MyMap/MyMap.jsx
@@ -0,0 +1,95 @@
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+} from "react";
+import { useSelector } from "react-redux";
+import { GoogleMap } from "@react-google-maps/api";
+import EventForm from "../../FormElements/EventForm/EventForm";
+import { useLoading } from "../../../contexts/LoadingContext";
+
+const containerStyle = {
+  width: "100%",
+  height: "100vh",
+};
+
+const MyMap = () => {
+  const MAP_ID = import.meta.env.VITE_GOOGLE_MAPS_MAP_ID;
+  const mapRef = useRef(null);
+  const { setLoading } = useLoading();
+  const [mapCenter, setMapCenter] = useState(null);
+  const [showEventForm, setShowEventForm] = useState(false);
+  const [eventLocation, setEventLocation] = useState(null);
+  const options = useMemo(
+    () => ({
+      mapId: MAP_ID,
+      disableDefaultUI: true,
+      clickableIcons: false,
+      zoomControl: true,
+    }),
+    []
+  );
+  const onLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  // Get current user's location from Redux store to set map center if user denies location access
+  const userLocationFromDb = useSelector(
+    (state) => state.auth.user.location.coordinates
+  );
+
+  const handleMapClick = (event) => {
+    setEventLocation({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+    setShowEventForm(true);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setMapCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        setLoading(false);
+      },
+      (error) => {
+        console.error(error);
+        setMapCenter({
+          lat: userLocationFromDb[1],
+          lng: userLocationFromDb[0],
+        });
+        setLoading(false);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  }, [setLoading, userLocationFromDb]);
+
+  return (
+    <>
+      <GoogleMap
+        center={mapCenter}
+        zoom={14}
+        onClick={handleMapClick}
+        mapContainerStyle={containerStyle}
+        options={options}
+        onLoad={onLoad}
+      ></GoogleMap>
+      {showEventForm && (
+        <EventForm
+          location={eventLocation}
+          onClose={() => setShowEventForm(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default MyMap;
--- LinguaConnect/client/src/pages/Map/Map.jsx
+++ LinguaConnect/client/src/pages/Map/Map.jsx
@@ -1,62 +1,52 @@
-"use client";
 import React, { useState } from "react";
-import { APIProvider } from "@vis.gl/react-google-maps";
 import Drawer from "@mui/material/Drawer";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import {
-  MiniNavBar,
-  MinimalSelect,
-  EventList,
-  GoogleMap,
-} from "../../components";
+import { MiniNavBar, MinimalSelect, EventList, MyMap } from "../../components";
 
 const Map = () => {
-  const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   const [selectedCategory, setSelectedCategory] = useState("createdByMe");
 
   return (
-    <APIProvider apiKey={API_KEY}>
-      <Box sx={{ display: "flex", height: "100vh" }}>
-        <Drawer
-          sx={{
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Drawer
+        sx={{
+          width: "20%",
+          height: "auto",
+          maxHeight: "100vh",
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
             width: "20%",
-            height: "auto",
-            maxHeight: "100vh",
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: "20%",
-              backgroundColor: "var(--primary-color)",
-            },
+            backgroundColor: "var(--primary-color)",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <MiniNavBar />
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            paddingTop: 2,
+            textAlign: "center",
+            fontWeight: "bold",
           }}
-          variant="permanent"
-          anchor="left"
         >
-          <MiniNavBar />
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{
-              paddingTop: 2,
-              textAlign: "center",
-              fontWeight: "bold",
-            }}
-          >
-            Events
-          </Typography>
-          <MinimalSelect
-            selectedCategory={selectedCategory}
-            setSelectedCategory={setSelectedCategory}
-          />
-          <Divider />
-          <EventList selectedCategory={selectedCategory} />
-        </Drawer>
-        <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
-          <GoogleMap />
-        </Box>
+          Events
+        </Typography>
+        <MinimalSelect
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Divider />
+        <EventList selectedCategory={selectedCategory} />
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
+        <MyMap />
       </Box>
-    </APIProvider>
+    </Box>
   );
 };
 
