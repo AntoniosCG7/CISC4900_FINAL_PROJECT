5a76df9d39934f885a0d61f1cf0a3e93aa82458f
Antonios Chatzinikolas <antonis_132000@hotmail.com>
2023-11-12 14:29:10 -0500

    Add custom theme to DateSelect component

--- LinguaConnect/client/src/components/FormElements/DateSelect/DateSelect.jsx
+++ LinguaConnect/client/src/components/FormElements/DateSelect/DateSelect.jsx
@@ -2,41 +2,64 @@ import React, { useState } from "react";
 import dayjs from "dayjs";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const DateSelect = () => {
   const [date, setDate] = useState(dayjs());
   const tomorrow = dayjs().add(1, "day");
 
+  const theme = createTheme({
+    components: {
+      MuiPickersDay: {
+        styleOverrides: {
+          root: {
+            fontSize: "0.9rem",
+            "&:hover": {
+              backgroundColor: "var(--primary-color)",
+            },
+            "&.Mui-selected": {
+              backgroundColor: "var(--primary-color)",
+              color: "var(--secondary-color)",
+              fontWeight: "bold",
+            },
+          },
+        },
+      },
+    },
+  });
+
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          sx={{
-            "& .MuiInputBase-root": {
-              border: "3px solid var(--primary-color)",
-              backgroundColor: "#e0e0e0",
-              fontSize: "1rem",
-              fontWeight: "400",
-              "&:hover": {
-                cursor: "pointer",
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            sx={{
+              "& .MuiInputBase-root": {
+                border: "3px solid var(--primary-color)",
+                backgroundColor: "#e0e0e0",
+                fontSize: "1rem",
+                fontWeight: "400",
+                "&:hover": {
+                  cursor: "pointer",
+                },
               },
-            },
-            "& .MuiOutlinedInput-notchedOutline": {
-              border: "none",
-            },
-            "&:hover .MuiOutlinedInput-notchedOutline": {
-              border: "none",
-            },
-            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-              border: "none",
-            },
-          }}
-          value={date}
-          onChange={(newDate) => setDate(newDate)}
-          minDate={tomorrow}
-          required
-        />
-      </LocalizationProvider>
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+            value={date}
+            onChange={(newDate) => setDate(newDate)}
+            minDate={tomorrow}
+            required
+          />
+        </LocalizationProvider>
+      </ThemeProvider>
     </>
   );
 };
